0ea61af58af9f39e43794178527dc050c8ce5b09
Ubuntu <smsr3f@sylis.wqis2b5oumauremoucg1qzxpae.gx.internal.cloudapp.net>
2016-04-11 02:46:31 +0000

    login page updates

--- index.php
+++ index.php
@@ -2,61 +2,50 @@
 	session_start();
 ?>
 <html>
-<head>
-<!--  I USE BOOTSTRAP BECAUSE IT MAKES FORMATTING/LIFE EASIER -->
+	<head>
+		<!--  I USE BOOTSTRAP BECAUSE IT MAKES FORMATTING/LIFE EASIER -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"><!-- Optional theme -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script><!-- Latest compiled and minified JavaScript -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<title>LinkedOut</title>
-    
-    <style>
-                  
+
+		<title>LinkedOut</title>
+
+    	<style>
             #loginbox{
-                
               background-color: #cccccc;
-                
             }
             .w3-btn{
                 width:150px;
             }
-        </style>
-        
-    
-    
-    
-</head>
+		</style>
+	</head>
+	<body>
+	    <div id="rownav">
+	        <div class="col-sm-12">
+	            <nav class="navbar navbar-inverse navbar-fixed-top">
+	              <div class="container-fluid">
+	                <div class="navbar-header">
+	                  <a class="navbar-brand" href="#">LinkedOut</a>
+	                </div>
+	              </div>
+	            </nav>
+	        </div>
+	    </div>
 
-<body>
+	    <div id="rowspace">
+	        <div class="col-sm-12">
+	            <div class="page-header">
+	                <h1></h1>
+	            </div>
+	        </div>
+	    </div>
 
-    <div id="rownav">
-        <div class="col-sm-12">
-            <nav class="navbar navbar-inverse navbar-fixed-top">
-              <div class="container-fluid">
-                <div class="navbar-header">
-                  <a class="navbar-brand" href="#">LinkedOut</a>
-                </div>
-              </div>
-            </nav>
-        
-        </div>
-    </div>
-    
-    <div id="rowspace">
-        <div class="col-sm-12">
-            <div class="page-header">
-                <h1></h1>
-            </div>
-        
-        </div>
-    </div>
-    
-    
-    <div class="container">
+    	<div class="container">
 			<div class="row">
 				<div class="col-md-4 col-sm-4 col-xs-3"></div>
-                
+
                 <div id="loginbox">
 				<div class="col-md-4 col-sm-4 col-xs-6">
 					<h2>Login</h2>
@@ -73,75 +62,38 @@
                                 <!--<input class=" btn btn-info" type="submit" name="logout" value="Logout"/>-->
 						</div>
 					</form>
-                
 				</div>
-                
 			</div>
         </div>
-        
-        
-        
+
 			<?php
-			
-							
 				if(isset($_POST['submit'])) { // Was the form submitted?
-					
+					include "./secure/db.conf";
                     //must add connection to db
-					$link = mysqli_connect("#","#","#","#") or die ("Connection Error " . mysqli_error($link));
-					//password salted and Hashed
-					
-					$sql = 'SELECT salt, hashed_password, usertype FROM users WHERE username = "';
+					$link = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die ("Connection Error " . mysqli_error($link));
+					//password hashed
+
+					$sql = 'SELECT user_id, hashed_password FROM users WHERE username = "';
 					$query=$sql . $_POST['username'].'";';
 					$result = mysqli_query($link, $query);
 						$row = mysqli_fetch_assoc($result);
-						$localhash = sha1( $row['salt'] . $_POST['password'] );
-						if ($localhash == $row['hashed_password'])
+						if (password_verify($_POST['password'], $row['hashed_password']))
 						{
 							echo 'You logged in!';
 							// Set session variables
 							$_SESSION['username'] = $_POST['username'];
-							$_SESSION['usertype'] = $row['usertype'];
+							$_SESSION['user_id'] = $row['user_id'];
 							$_SESSION['islogin'] = '1';
-							
-							
-							
-							date_default_timezone_set("America/Chicago");
-							$time2=date('Y-m-d H:i:s');
-							$query2='UPDATE logins SET time_of_login="' . $time2 . '" WHERE userName = "' . $_POST['username'].'";';
-							$query3='UPDATE logins SET loginTimes=loginTimes+1 WHERE userName = "' . $_POST['username'].'";';
-							mysqli_query($link, $query2);
-							mysqli_query($link, $query3);
-							
-							
-							if($_SESSION['usertype']=='c')
-							{
-								header("Location: index.php");
-							}
-							else
-							{
-								header("Location: main.php");
-							}
-							
+
+							header("Location: home.php");
 						}
 						else
 						{
-							echo 'Password error!';
-							
+							echo 'Username and/or Password are incorrect!';
 						}
-						
-						
-   
-    
-					
 				}
-			
 			?>
 		</div>
-    
-    
-    
-    
-    
     </td>
   </tr>
 </table>

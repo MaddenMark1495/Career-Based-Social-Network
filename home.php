0ee7a22fd8bcb023b0957fa2247ea50baa428d87
strothmannsm <smsr3f@mail.missouri.edu>
2016-04-14 18:14:33 -0500

    home php test

--- home.php
+++ home.php
@@ -1,11 +1,15 @@
 <?php
 	session_start();
+
+	if(!$_SESSION['islogin']) {
+		header("Location: index.php");
+	}
 ?>
 <html>
 	<head>
 		<title>LinkedOut</title>
 	</head>
 	<body>
-		<h1>You're logged in as <?php print $_SESSION['user'];?></h2>
+		<h1>You're logged in as <?php print $_SESSION['username'];?></h2>
 	</body>
 </html>

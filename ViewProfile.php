fadc7f982e918dfa1c4007e60b673d411d92c460
strothmannsm <smsr3f@mail.missouri.edu>
2016-04-21 14:19:03 -0500

    commit

--- ViewProfile.php
+++ ViewProfile.php
@@ -169,10 +169,9 @@
             	<h2>Background</h2>
             	<h4>Summary</h4>
 <?php
-	$sql = "SELECT summary from users where user_id=$view_id";
-	//$result = $conn->query($sql);
+	echo "<p id='summary'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>";
+	//echo "<p id='summary'>" . $row['summary'] . "</p>";
 ?>
-            	<p id="summary">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,</p>
 				<hr>
 				<h4>Experience</h4>
 <?php

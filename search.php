e53f79ae6e0699cec87114888ad2a232d9ad69f8
luke-moss <lamz55@mail.missouri.edu>
2016-04-26 06:32:44 -0500

    Search Page
    Added search functionality to the website.

---
+++ search.php
@@ -0,0 +1,155 @@
+<?php
+	session_start();
+	if(!$_SESSION['islogin']){
+		header("Location: index.php");
+	}
+	if(isset($_POST['view'])){
+		$id = $_POST['view_id'];
+		header("Location: ViewProfile.php?user_id=$id");
+	}
+	$uid = $_SESSION['user_id'];
+	require "../secure/db.conf";
+	if(!$link = new mysqli($dbhost, $dbuser, $dbpass, $dbname)){
+		die("Connection Error: " . $link->error);
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Search</title>
+		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open Sans">
+		<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+		<style>
+			.nav-pills > li.active > a, .nav-pills > li.active > a:hover, .nav-pills > li.active > a:focus{
+				color: black;
+				background-color: #33ff77
+			}
+			#background{
+				border: 1px solid grey;
+				background-color: #FFFFFF
+			}
+			.row{
+				margin-bottom: 100px
+			}
+			body{
+				background-color: #f2f2f2
+			}
+			h4, h5, p{
+				padding-left: 10px
+			}
+		</style>
+	</head>
+	<body>
+		<div class="row" id="row0">
+			<div class="col-sm-12">
+				<nav class="navbar navbar-inverse navbar-fixed-top">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" id="Icon" href="index.php">LinkedOut</a>
+						</div>
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<form class="navbar-form navbar-left" role="search" method="post">
+								<div class="form-group">
+									<input name="search" type="text" class="form-control" placeholder="People, jobs, Etc." value='<?=$_POST['search']?>' required>
+								</div>
+								<button type="submit" class="btn btn-default">Search</button>
+							</form>
+							<ul class="nav navbar-nav navbar-right">
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$_SESSION['username'];?><span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="ViewProfile.php?user_id=<?php echo $_SESSION['user_id'];?>">View Profile</a></li>
+										<li><a href="editprofile.php">Edit Profile</a></li>
+										<li><a href="connections.php">Connections</a></li>
+										<li><a href="logout.php">Logout</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="Top10.php">Top 10 Users</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+		<center>
+			<?php
+				if(isset($_POST['search'])){
+					$search = $_POST['search'];
+					echo "<h1>Search results for \"".$search."\"</h1>";
+					$sql="SELECT * FROM users WHERE fname LIKE '%".$search."%' OR lname LIKE '%".$search."%'";
+					if($result = $link->query($sql)){
+						$num = $result->num_rows;
+						echo "Number of results: ".$num."<br><br>";
+						while($row = $result->fetch_assoc()){
+							//Name
+							echo "<b>Name: ".$row['fname']." ".$row['lname']."</b>";
+							//Occupation
+							$title = $row['cur_title'];
+							$company = $row['cur_company'];
+							if($title or $company){
+								echo "<br>Occupation: ";
+								if($title){
+									echo $title;
+									if($company){
+										echo ", ";
+									}
+								}
+								if($company){
+									echo $company;
+								}
+							}
+							//Location
+							$city = $row['city'];
+							$state_id = $row['state'];
+							if($state_id){
+								$output = $link->query("SELECT state FROM states WHERE idstates=$state_id");
+								$state_row = $output->fetch_assoc();
+								$state = $state_row['state'];
+								$output->free();
+							}
+							if($city or $state){
+								echo "<br>Location: ";
+								if($city){
+									echo $city;
+									if($state){
+										echo ", ";
+									}
+								}
+								if($state){
+									echo $state;
+								}
+							}
+							?>
+							<!--View Profile-->
+							<div id="button">
+								<form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+									<input name="view_id" type="hidden" value="<?=$row['user_id'];?>">
+									<input name="view" type="submit" class="w3-btn w3-hover-green" value="View Profile">
+								</form>
+							</div>
+							<br><br>
+							<?php
+						}
+						$result->free();
+					}else{
+						echo "Query Error: ".$link->error;
+					}
+				}else{
+					echo "Error: no search input";
+				}
+				$link->close();
+			?>
+		</center>
+	</body>
+</html>
